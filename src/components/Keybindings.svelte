0afa72f68c5326bb23d1b6915ab7a485f1d26d3d
Kevin Loughead <kvnloughead@gmail.com>
2023-10-08 18:10:10 -0400

    fix: removed pointless type

--- src/components/Keybindings.svelte
+++ src/components/Keybindings.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
-  type KeyCombo =
-    | "ctrl"
-    | "alt"
-    | "shift"
-    | "meta"
-    | `${ModKey}+${string}`
-    | string;
-  type ModKey = "ctrl" | "alt" | "shift" | "meta";
-
-  /** An object that maps keybindings to their respective handlers. Keys can optionally be preceded by a + separated sequence of modifiers. For example, 'n', 'ctrl+n', and 'ctrl+shift+n' are all acceptable. Allowed modifiers are ctrl, shift, alt, and meta. */
-  export let handlers: Record<KeyCombo, (evt: KeyboardEvent) => void>;
+  /** An object that maps keybindings to their respective handlers. Keys can optionally be preceded by a + separated sequence of modifiers. For example, 'n', 'ctrl+n', and 'ctrl+shift+n' are all acceptable. Allowed modifiers are ctrl, shift, alt, and meta.
+   */
+  export let handlers: Record<string, (evt: KeyboardEvent) => void>;
 
   const handleKeydown = (evt: KeyboardEvent) => {
     const target = evt.target as HTMLElement;
@@ -26,7 +18,7 @@
     }
   };
 
-  const matchesKeyCombo = (evt: KeyboardEvent, keyCombo: KeyCombo) => {
+  const matchesKeyCombo = (evt: KeyboardEvent, keyCombo: string) => {
     const modifiers = ["ctrl", "alt", "shift", "meta"];
     const keys = keyCombo.toLowerCase().split("+");
     const key = keys.pop();
